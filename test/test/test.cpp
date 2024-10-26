648dee8259234b099c31771d142111a4c5f5f5ef
sbstndb <sebastienduboid4@gmail.com>
2024-10-26 12:26:27 +0200

    Add tests

---
+++ test/test/test.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+#include "gtest/gtest.h"
+
+// define simple add function 
+//
+int add_int(int a, int b){
+	return a + b ; 
+}
+
+TEST(Addtest, PositiveNumbers){
+	int a = 1 ; 
+	int b = 2 ; 
+	EXPECT_EQ(3, add_int(a,b)) ;
+}
+
+TEST(Addtest, Fauxnegatif){
+        int a = 1 ;
+        int b = 2 ;
+        EXPECT_EQ(4, add_int(a,b)) ;
+}
+
+
+
+
